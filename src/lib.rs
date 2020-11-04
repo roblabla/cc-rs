43e594e0455e178ceadf28fafde2902fc108efa3
roblabla <unfiltered@roblab.la>
2020-11-04 12:47:12 +0000

    Forcibly enable /guard:cf when calling C compiler

--- src/lib.rs
+++ src/lib.rs
@@ -1389,6 +1389,8 @@ impl Build {
                     }
                 };
                 cmd.push_cc_arg(crt_flag.into());
+                // TODO: Detect if control-flow-guard is enabled.
+                cmd.push_cc_arg("-guard:cf".into());
 
                 match &opt_level[..] {
                     // Msvc uses /O1 to enable all optimizations that minimize code size.
